7bc1cf8c25880673114b345eefcabcfac683f5bf
OmprakashPatidar1 <patidaromprakash051@gmail.com>
2023-05-19 23:40:38 +0530

    booking App

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const sequelize = require('./util/database');
+
+var cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const userRoutes = require('./routes/user');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(userRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+ .sync()
+ .then(result => {
+    //console.log(result);
+    app.listen(3000);
+ })
+ .catch(err => {
+    console.log(err);
+ })
+
+//app.listen(3000);
